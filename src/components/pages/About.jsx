27e49895dfb5ee232c20d5500bf4622721c2940a
root-0101 <unofficially111@gmail.com>
2024-09-16 00:36:29 +0530

    update about

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -9,25 +9,25 @@ import { motion } from "framer-motion";
 function About() {
   return (
     <>
-      <div className="h-[90%] mt-16 w-full flex flex-col justify-end fixed right-0 border-t-[1px] border-gray-500 overflow-auto">
+      <div className="h-[90%] mt-8 md:mt-16 w-full flex flex-col justify-end fixed right-0 border-t-[1px] border-gray-500 overflow-x-hidden">
         <div className=" h-[87%] w-full flex flex-col items-center">
 
           {/* About section */}
-          <div className="w-[80%] flex items-center gap-24">
-            <motion.img initial={{ opacity: 1 }} whileHover={{ opacity: 1, scale: 1.1 }} transition={{ duration: 0.2 }} className="h-80 w-[500px] shadow-lg shadow-[#1e1e1e] border-[1px] border-[#D1D5DB]" src={banner} alt="banner" />
-            <div className="text-[#1d1d1d] text-5xl font-bold">
-              <p>About</p>
-              <p className="text-lg text-gray-800 font-normal mt-8 ">BizAmplify is an innovative platform designed to empower micro and nano entrepreneurs. It offers personalized business recommendations, connects them with potential investors, and fosters a collaborative community. By addressing financial challenges and offering strategic insights, BizAmplify helps small businesses grow, compete, and succeed in today’s rapidly evolving marketplace.</p>
+          <div className="md:w-[80%] w-[90vw] flex flex-col md:flex-row  items-center md:gap-24 gap-8">
+            <motion.img initial={{ opacity: 1 }} whileHover={{ opacity: 1, scale: 1.1 }} transition={{ duration: 0.2 }} className="h-80 md:w-[35vw] shadow-lg shadow-[#1e1e1e] border-[1px] border-[#D1D5DB]" src={banner} alt="banner" />
+            <div className="text-[#1d1d1d] text-5xl font-bold md:w-[60vw] w-[90vw]">
+              <p className="text-center md:text-left">About</p>
+              <p className="text-lg text-gray-800 font-normal mt-8 md:p-0 p-4">BizAmplify is an innovative platform designed to empower micro and nano entrepreneurs. It offers personalized business recommendations, connects them with potential investors, and fosters a collaborative community. By addressing financial challenges and offering strategic insights, BizAmplify helps small businesses grow, compete, and succeed in today’s rapidly evolving marketplace.</p>
 
-              <motion.a href="#team" className="cursor-pointer scroll-smooth" ><motion.button initial={{ opacity: 1 }} whileHover={{ opacity: 1, scale: 1.05 }} transition={{ duration: 0.2 }}  className="bg-[#67da20] p-2 rounded-lg mt-8 text-lg font-semibold w-56">Contact Us</motion.button></motion.a>
+              <motion.a href="#team" className="cursor-pointer scroll-smooth" ><motion.button initial={{ opacity: 1 }} whileHover={{ opacity: 1, scale: 1.05 }} transition={{ duration: 0.2 }}  className="bg-[#67da20] p-2 rounded-lg mt-4 text-lg font-semibold md:w-56 mx-4 md:mx-0 w-[80vw]">Contact Us</motion.button></motion.a>
             </div>
           </div>
 
 
           {/* Team section */}
-          <div className="w-[80%] mt-32 gap-5 flex flex-col items-center p-8 rounded-md bg-[#F7F7F7] border-2 mb-10 border-[#d1d5db] " id="team">
+          <div className="w-[80%] md:mt-32 mt-20 gap-5 flex flex-col items-center p-8 rounded-md bg-[#F7F7F7] border-2 mb-10 border-[#d1d5db] " id="team">
             <p className="text-3xl font-bold ">Meet Our Team</p>
-            <div className="grid grid-cols-3 gap-8 ">
+            <div className="grid md:grid-cols-3 gap-8 md:gap-8">
 
               
               <motion.div initial={{ opacity: 1 }} whileHover={{ opacity: 1, scale: 1.05 }} transition={{ duration: 0.1 }} className="h-96 w-[300px] rounded-lg bg-[#1e1e1e] flex flex-col items-center justify-center shadow-md border-[1px] border-[#D1D5DB] shadow-[#3b3b3b]" >
@@ -65,15 +65,15 @@ function About() {
 
 
           {/* links section */}
-          <div className="w-[80%] mt-10 gap-0 flex justify-center items-center p-0 rounded-md bg-[#F7F7F7] border-2 mb-10 border-[#d1d5db] ">
-            <p className="text-3xl w-[32%] font-bold ">Get our project details on: </p>
+          <div className="w-[80%] mt-10  gap-0 flex justify-center items-center p-0 rounded-md bg-[#F7F7F7] border-2 mb-10 border-[#d1d5db] ">
+            <p className="md:text-3xl text-xl md:w-[32%] w-[70%] text-center font-bold">Get our project details on: </p>
             <div className="w-[20%] p-4 rounded-lg">
 
                <div className="flex items-center justify-center">
                 
-                <motion.a initial={{ opacity: 1 }} whileHover={{ opacity: 1, scale: 1.05 }} transition={{ duration: 0.1 }} href="https://github.com/Shubham-1068/Hackers-Hive-HIT-A-THON" target="_blank" className="text-md border-2 border-slate-500 h-32 w-32 flex flex-col items-center justify-around rounded-lg hover:bg-slate-200">
-                  <Github className="border-2 border-slate-900 rounded-full p-1 mt-2" size={60} />
-                  <p className=" text-lg font-semibold">Github</p>
+                <motion.a initial={{ opacity: 1 }} whileHover={{ opacity: 1, scale: 1.05 }} transition={{ duration: 0.1 }} href="https://github.com/Shubham-1068/Hackers-Hive-HIT-A-THON" target="_blank" className="text-md border-2 border-slate-500 md:h-32 md:w-32 flex flex-col items-center justify-around rounded-lg hover:bg-slate-200 h-20 w-20">
+                  <Github className="md:border-2 border-slate-900 rounded-full p-1 mt-2" size={60} />
+                  <p className=" md:text-lg font-semibold">Github</p>
                 </motion.a>
                </div>
 
